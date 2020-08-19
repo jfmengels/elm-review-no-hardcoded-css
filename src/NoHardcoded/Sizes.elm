b3c9efb8442f26403ca1fb8cd64bef09c964b818
Jeroen Engels <jfm.engels@gmail.com>
2020-08-19 19:28:12 +0200

    Forbids using CSS size functions

--- src/NoHardcoded/Sizes.elm
+++ src/NoHardcoded/Sizes.elm
@@ -6,7 +6,10 @@ module NoHardcoded.Sizes exposing (rule)
 
 -}
 
-import Review.Rule as Rule exposing (Rule)
+import Elm.Syntax.Expression as Expression exposing (Expression)
+import Elm.Syntax.Node as Node exposing (Node)
+import Review.Rule as Rule exposing (Error, Rule)
+import Set exposing (Set)
 
 
 {-| Reports... REPLACEME
@@ -39,12 +42,51 @@ This rule is not useful when REPLACEME.
 You can try this rule out by running the following command:
 
 ```bash
-elm-review --template jfmengels/elm-review-no-hardcoded-css/example --rules NoHardcoded.Sizes
+elm - review --template jfmengels/elm-review-no-hardcoded-css/example --rules NoHardcoded.Sizes
 ```
 
 -}
 rule : Rule
 rule =
     Rule.newModuleRuleSchema "NoHardcoded.Sizes" ()
-        -- Add your visitors
+        |> Rule.withSimpleExpressionVisitor expressionVisitor
         |> Rule.fromModuleRuleSchema
+
+
+expressionVisitor : Node Expression -> List (Error {})
+expressionVisitor node =
+    case Node.value node of
+        Expression.FunctionOrValue [ "Css" ] name ->
+            if Set.member name sizeFunctions then
+                [ Rule.error
+                    { message = "REPLACEME"
+                    , details = [ "REPLACEME" ]
+                    }
+                    (Node.range node)
+                ]
+
+            else
+                []
+
+        _ ->
+            []
+
+
+sizeFunctions : Set String
+sizeFunctions =
+    Set.fromList
+        [ "px"
+        , "em"
+        , "pt"
+        , "ex"
+        , "ch"
+        , "rem"
+        , "vh"
+        , "vw"
+        , "vmin"
+        , "vmax"
+        , "mm"
+        , "cm"
+        , "inches"
+        , "pc"
+        ]
--- tests/NoHardcoded/SizesTest.elm
+++ tests/NoHardcoded/SizesTest.elm
@@ -5,20 +5,54 @@ import Review.Test
 import Test exposing (Test, describe, test)
 
 
+message : String
+message =
+    "REPLACEME"
+
+
+details : List String
+details =
+    [ "REPLACEME" ]
+
+
 all : Test
 all =
     describe "NoHardcoded.Sizes"
-        [ test "should report an error when REPLACEME" <|
-            \() ->
-                """module A exposing (..)
-a = 1
-"""
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error
-                            { message = "REPLACEME"
-                            , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
-                            }
-                        ]
+        [ testForSize "px"
+        , testForSize "em"
+        , testForSize "pt"
+        , testForSize "ex"
+        , testForSize "ch"
+        , testForSize "rem"
+        , testForSize "vh"
+        , testForSize "vw"
+        , testForSize "vmin"
+        , testForSize "vmax"
+        , testForSize "mm"
+        , testForSize "cm"
+        , testForSize "inches"
+        , testForSize "pc"
         ]
+
+
+testForSize : String -> Test
+testForSize functionName =
+    test ("should report an error when using Css." ++ functionName) <|
+        \() ->
+            """module A exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css)
+import Css
+
+view = div [ css [ Css.width (Css."""
+                ++ functionName
+                ++ """ 1) ] ]
+"""
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ Review.Test.error
+                        { message = message
+                        , details = details
+                        , under = "Css." ++ functionName
+                        }
+                    ]
